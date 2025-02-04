d894c9cc17141a01f0c29c5495563ce36df8347c
osd-chhutra <osd-chhutra@dc-svr-upgcli03.MEF.LOCAL>
2025-02-04 10:06:09 +0700

    fix bug

--- src/main/java/com/fmis/fmis_proxy_intf/fmis_proxy_intf/controller/TestController.java
+++ src/main/java/com/fmis/fmis_proxy_intf/fmis_proxy_intf/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.fmis.fmis_proxy_intf.fmis_proxy_intf.controller;
 import com.fmis.fmis_proxy_intf.fmis_proxy_intf.model.Test;
 import com.fmis.fmis_proxy_intf.fmis_proxy_intf.service.TestService;
-import com.fmis.fmis_proxy_intf.fmis_proxy_intf.util.StandardResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+//import com.fmis.fmis_proxy_intf.fmis_proxy_intf.util.StandardResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,27 +17,25 @@ public class TestController {
     public TestController(TestService testService) {
         this.testService = testService;
     }
-
     @GetMapping("/test")
-    public StandardResponse test() {
-        return StandardResponse.success("Hello World111", null);
+    public String test() {
+        return "Hello World";
     }
-
     @GetMapping("/getname")
     public Test getTestByTestName(@RequestParam String name) {
         Test test = testService.test(name);
 
         return test;
     }
-    @GetMapping("/getall")
-    public StandardResponse getAll() {
-        List<Test> testList = testService.getAllTests();
-        if (testList != null && !testList.isEmpty()) {
-            return StandardResponse.success("Tests found", testList);
-        } else {
-            return StandardResponse.notFound("No tests found");
-        }
-    }
+//    @GetMapping("/getall")
+//    public StandardResponse getAll() {
+//        List<Test> testList = testService.getAllTests();
+//        if (testList != null && !testList.isEmpty()) {
+//            return StandardResponse.success("Tests found", testList);
+//        } else {
+//            return StandardResponse.notFound("No tests found");
+//        }
+//    }
 
     @PostMapping("/add")
     public void  save(@RequestBody Test test){
