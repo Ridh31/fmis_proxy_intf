82e9ee642617c178e668b46b5e204e17809f86ae
Ridh31 <himroungridh2001@gmail.com>
2025-05-23 14:59:43 +0700

    [23-05-2025] - Security: Config CORS.

--- src/main/java/com/fmis/fmis_proxy_intf/fmis_proxy_intf/config/SecurityConfig.java
+++ src/main/java/com/fmis/fmis_proxy_intf/fmis_proxy_intf/config/SecurityConfig.java
@@ -151,7 +151,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of(
+                "https://dev-fmis-intf.fmis.gov.kh",
+                "http://10.10.3.52"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
