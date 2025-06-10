fd56e87724e5ab4f9e05db365c63249d3a505cd6
Ridh31 <himroungridh2001@gmail.com>
2025-06-10 11:32:30 +0700

    [10-06-2025] - Application: Improve Syntax.

--- src/main/java/com/fmis/fmis_proxy_intf/fmis_proxy_intf/FmisProxyIntfApplication.java
+++ src/main/java/com/fmis/fmis_proxy_intf/fmis_proxy_intf/FmisProxyIntfApplication.java
@@ -4,11 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * Main application class for the FMIS Proxy Interface.
+ * Extends {@link SpringBootServletInitializer} to support traditional deployment (e.g., WAR files).
+ */
 @SpringBootApplication
 public class FmisProxyIntfApplication extends SpringBootServletInitializer {
 
+	/**
+	 * Main method which acts as the starting point of the Spring Boot application.
+	 *
+	 * @param args Command-line arguments passed during application startup.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(FmisProxyIntfApplication.class, args);
 	}
-
 }
